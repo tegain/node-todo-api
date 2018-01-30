4017e188078a070712642ff2365c744582ea66be
Thomas Egain <thomas.egain@gmail.com>
2018-01-30 08:26:03 +0100

    Add hosted database config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Tell Mongoose which Promise library to use
 // Here, use the built-in library
 mongoose.Promise = global.Promise;
 // Mongoose connect to DB
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const DBURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.envDB_HOST}/node-todo-api`;
+mongoose.connect(DBURI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { User } = require('./models/user');
 const { ObjectID } = require('mongodb');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -63,8 +64,8 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => {
-	console.log('Started on port 3000');
+app.listen(port, () => {
+	console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
