0424c364604087dfa8e535cfb7494d5df30e0113
Thomas Egain <thomas.egain@gmail.com>
2018-01-31 13:59:00 +0100

    Add tests for PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -6,8 +6,17 @@ const { Todo } = require('../models/todo');
 
 // Create dummy todos
 const todos = [
-	{ _id: new ObjectID(), text: 'First test todo' },
-	{ _id: new ObjectID(), text: 'Second test todo' }
+	{
+		_id: new ObjectID(),
+		text: 'First test todo',
+		completed: false
+	},
+	{
+		_id: new ObjectID(),
+		text: 'Second test todo',
+		completed: true,
+		completedAt: 12345
+	}
 ];
 
 // Drop database before every test
@@ -147,6 +156,40 @@ describe('DELETE /todos/:id', () => {
 		request(app)
 			.delete(`/todos/${id}`)
 			.expect(404)
+			.end(done);
+	});
+});
+
+describe('PATCH /todos/:id', () => {
+	it('should update the todo', (done) => {
+		const id = todos[0]._id.toHexString();
+		const text = 'Updated first test todo';
+
+		request(app)
+			.patch(`/todos/${id}`)
+			.send({ text, completed: true })
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBeA('number');
+			})
+			.end(done);
+	});
+
+	it('should clear completedAt when todo is not completed', (done) => {
+		const id = todos[1]._id.toHexString();
+		const text = 'Updated second test todo';
+
+		request(app)
+			.patch(`/todos/${id}`)
+			.send({ text, completed: false })
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toNotExist();
+			})
 			.end(done);
 	});
 });
