c224873a8035d83e3597e47289d25653f3daa36f
Thomas Egain <thomas.egain@gmail.com>
2018-01-31 20:08:24 +0100

    Fix MongoDB URI for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,10 +5,6 @@ const mongoose = require('mongoose');
 // Here, use the built-in library
 mongoose.Promise = global.Promise;
 // Mongoose connect to DB
-const DB_REMOTE_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/node-todo-api`;
-const DB_LOCAL_URI = 'mongodb://localhost:27017/TodoApp';
-const DB_CONNECT_URI = process.env.DB_USER ? DB_REMOTE_URI : DB_LOCAL_URI;
-
-mongoose.connect(DB_CONNECT_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
